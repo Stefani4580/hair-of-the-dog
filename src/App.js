8cc823ce58787a1a2e922842441dd8550f9f78f0
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-30 11:25:08 -0400

    Added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Main from "./components/Main"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hair of the Dog</h1>
+      <Main />
     </div>
   );
 }
