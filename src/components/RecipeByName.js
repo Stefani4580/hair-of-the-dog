b78a309dc81add169919ee519de1629da56b154f
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-31 10:28:10 -0400

    Added form template

--- src/components/RecipeByName.js
+++ src/components/RecipeByName.js
@@ -1,24 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+// React Bootstrap 4
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default class RecipeByName extends Component {
-    constructor(){
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            data: []
-        }
-    }
+    this.state = {
+      data: [],
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-
-
-    render() {
-
-
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
