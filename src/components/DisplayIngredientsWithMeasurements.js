d182de467fbd53dff33dc51aa15346ab946bf7b0
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-31 17:07:43 -0400

    Display measurements

--- src/components/DisplayIngredientsWithMeasurements.js
+++ src/components/DisplayIngredientsWithMeasurements.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 function DisplayIngredientsWithMeasurements() {
+    // Measurements and Ingredients are in sychronized arrays.
+    let index = 0;
+    const listOfIngredMeas = props.ingredient.map((ingredient)=>{
+        <ListGroup horizontal>
+        <ListGroup.Item>{(measurement[index]=== null)? "" : measurement[index]}</ListGroup.Item>
+        <ListGroup.Item>{ingredient}</ListGroup.Item>
+        </ListGroup>
+
+    })
   return (
     <div>
-      <ListGroup horizontal>
-        <ListGroup.Item>This</ListGroup.Item>
-        <ListGroup.Item>ListGroup</ListGroup.Item>
         <ListGroup.Item>renders</ListGroup.Item>
         <ListGroup.Item>horizontally!</ListGroup.Item>
-      </ListGroup>
     </div>
   );
 }
