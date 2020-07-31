8297c2f0bffb99a5e84659c39a137bef4a1b4d6e
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-31 09:11:19 -0400

    Added state to RecipeByName

--- src/components/RecipeByName.js
+++ src/components/RecipeByName.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 
 export default class RecipeByName extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            data: []
+        }
+    }
+
+
+
+
     render() {
 
-        
+
         return (
             <div>
                 
