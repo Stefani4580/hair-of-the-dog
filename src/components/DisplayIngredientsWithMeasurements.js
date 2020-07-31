5f7ab37eaeb145a9ae28d67df2ae3213e8cd1c3e
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-31 17:10:23 -0400

    added props parameter

--- src/components/DisplayIngredientsWithMeasurements.js
+++ src/components/DisplayIngredientsWithMeasurements.js
@@ -1,21 +1,17 @@
 import React from "react";
 
-function DisplayIngredientsWithMeasurements() {
-    // Measurements and Ingredients are in sychronized arrays.
-    let index = 0;
-    const listOfIngredMeas = props.ingredient.map((ingredient)=>{
-        <ListGroup horizontal>
-        <ListGroup.Item>{(measurement[index]=== null)? "" : measurement[index]}</ListGroup.Item>
-        <ListGroup.Item>{ingredient}</ListGroup.Item>
-        </ListGroup>
-
-    })
-  return (
-    <div>
-        <ListGroup.Item>renders</ListGroup.Item>
-        <ListGroup.Item>horizontally!</ListGroup.Item>
-    </div>
-  );
+function DisplayIngredientsWithMeasurements(props) {
+  // Measurements and Ingredients are in sychronized arrays.
+  let index = 0;
+  const listOfIngredMeas = props.ingredient.map((ingredient) => {
+    <ListGroup horizontal>
+      <ListGroup.Item>
+        {measurement[index] === null ? "" : measurement[index]}
+      </ListGroup.Item>
+      <ListGroup.Item>{ingredient}</ListGroup.Item>
+    </ListGroup>;
+  });
+  return <div>{listOfIngredMeas} </div>;
 }
 
 export default DisplayIngredientsWithMeasurements;
