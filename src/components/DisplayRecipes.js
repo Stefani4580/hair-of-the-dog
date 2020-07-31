9b89e5519c1ab2547a22f7c52498e68e441d0040
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-31 16:17:33 -0400

    Testing putting a ListGroup in Card

--- src/components/DisplayRecipes.js
+++ src/components/DisplayRecipes.js
@@ -1,24 +1,27 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-
+import ListGroup from "react-bootstrap/ListGroup";
 
 function DisplayRecipes(props) {
+  console.log("This is props: ", props);
 
-    console.log("This is props: ", props);
-  
-  return(
-      <Card>
-        <Card.Img variant="top" src={props.strDrinkThumb} />
-        <Card.Body>
-          <Card.Title>{props.strDrink}</Card.Title>
-          <Card.Text>
-            {props.strInstructions}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
+  return (
+    <Card>
+      <Card.Img variant="top" src={props.strDrinkThumb} />
+      <Card.Body>
+        <Card.Title>{props.strDrink}</Card.Title>
+        <ListGroup horizontal>
+          <ListGroup.Item>This</ListGroup.Item>
+          <ListGroup.Item>ListGroup</ListGroup.Item>
+          <ListGroup.Item>renders</ListGroup.Item>
+          <ListGroup.Item>horizontally!</ListGroup.Item>
+        </ListGroup>
+        <Card.Text>{props.strInstructions}</Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">Last updated 3 mins ago</small>
+      </Card.Footer>
+    </Card>
   );
 }
 
