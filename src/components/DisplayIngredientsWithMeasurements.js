457bd3084d9f45b3d30fc8c414307d611c3a12f6
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-31 16:21:29 -0400

    Add component to display ing/mes

---
+++ src/components/DisplayIngredientsWithMeasurements.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DisplayIngredientsWithMeasurements() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DisplayIngredientsWithMeasurements
