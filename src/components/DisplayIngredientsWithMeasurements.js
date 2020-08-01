9da02dad1876c6807d814ed919718ad10c2ca173
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-31 20:30:43 -0400

    Trying to resolve unique key but no luck

--- src/components/DisplayIngredientsWithMeasurements.js
+++ src/components/DisplayIngredientsWithMeasurements.js
@@ -2,14 +2,15 @@ import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 
 function DisplayIngredientsWithMeasurements(props) {
-
-  const listOfIngredMeas = props.ingredients.map((ingredient) => {
-    return (<ListGroup horizontal>
-      <ListGroup.Item>{ingredient[1]}</ListGroup.Item>
-      <ListGroup.Item>
-        {ingredient[0] === null ? "" : ingredient[0]}
-      </ListGroup.Item>
-    </ListGroup>);
+  const listOfIngredMeas = props.ingredients.map((ingredient, id) => {
+    return (
+      <ListGroup horizontal>
+        <ListGroup.Item key={Math.floor(Math.random() * 1000)}>{ingredient[1]}</ListGroup.Item>
+        <ListGroup.Item key={Math.floor(Math.random() * 100)}>
+          {ingredient[0] === null ? "" : ingredient[0]}
+        </ListGroup.Item>
+      </ListGroup>
+    );
   });
   return <div>{listOfIngredMeas} </div>;
 }
