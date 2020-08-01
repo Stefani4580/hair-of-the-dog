9a1f105c3cfba2636f00f7b5ec92c5f282b2128a
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-31 21:15:48 -0400

    corrected name

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ import {
   Link
 } from "react-router-dom";
 
-export default function App() {
+export default function Main() {
   return (
     <Router>
       <div>
@@ -48,6 +48,3 @@ export default function App() {
 
 
 
-function Users() {
-  return <h2>Users</h2>;
-}
