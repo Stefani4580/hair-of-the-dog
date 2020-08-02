be41010359b0f665822636d13c1df5b83f3eb7bb
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-08-02 19:05:29 -0400

    Added Form template to Modal

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import auth from "firebase";
 
 // React Bootstrap 4
 import Container from "react-bootstrap/Container";
@@ -6,57 +7,74 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal"
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
 import "../App.css";
 
 const Home = (props) => {
-
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  return (
+    <div>
+      <Container fluid>
+        <Row>
+          <Col>
+            <h1 className="title">Welcome to the Hair of the Dog</h1>
+            <>
+              <Button variant="primary" onClick={handleShow}>
+                Sign Up
+              </Button>
 
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form>
+                    <Form.Group controlId="formBasicEmail">
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control type="email" placeholder="Enter email" />
+                      <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                      </Form.Text>
+                    </Form.Group>
 
-    return (
-      <div>
-        <Container fluid>
-          <Row>
-            <Col>
-              <h1 className="title">Welcome to the Hair of the Dog</h1>
-              <>
-                <Button variant="primary" onClick={handleShow}>
-                  Sign Up
-                </Button>
-
-                <Modal show={show} onHide={handleClose}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>
-                    Woohoo, you're reading this text in a modal!
-                  </Modal.Body>
-                  <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                      Close
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                      Submit
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                      Save Changes
-                    </Button>
-                  </Modal.Footer>
-                </Modal>
-              </>
+                  </Form>{" "}
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </>
 
-              <Image
-                fluid
-                src="https://images.unsplash.com/photo-1508253730651-e5ace80a7025?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-                alt=""
-              />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+            <Image
+              fluid
+              src="https://images.unsplash.com/photo-1508253730651-e5ace80a7025?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+              alt=""
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 export default Home;
