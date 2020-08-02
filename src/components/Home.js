1b68f69eb18c1e3795c145121ae0f4d91597d5b5
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-08-02 18:54:42 -0400

    Added Modal template.  Uncommented firebase JS

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,8 +12,35 @@ export default class Home extends Component {
         <Container fluid>
           <Row>
             <Col>
-            <h1 className="title">Welcome to the Hair of the Dog</h1>
-            <Image fluid src="https://images.unsplash.com/photo-1508253730651-e5ace80a7025?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""/>
+              <h1 className="title">Welcome to the Hair of the Dog</h1>
+              <>
+                <Button variant="primary" onClick={handleShow}>
+                  Sign Up
+                </Button>
+
+                <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    Woohoo, you're reading this text in a modal!
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                      Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                      Save Changes
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </>
+
+              <Image
+                fluid
+                src="https://images.unsplash.com/photo-1508253730651-e5ace80a7025?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+                alt=""
+              />
             </Col>
           </Row>
         </Container>
