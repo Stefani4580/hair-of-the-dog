966fdedb9466959f9dde42c95f7b9233fec6a307
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-08-02 19:23:58 -0400

    Customizing Form Template.

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,9 +14,31 @@ import "../App.css";
 
 const Home = (props) => {
   const [show, setShow] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    //Create User with Email and Password
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => {
+        // Handle Errors here.
+        alert(error.message);
+      });
+  };
+
+  const onEmailChange = (e) =>{
+    setEmail(e.target.value);
+  }
+
+  const onPasswordChange = (e) =>{
+    setPassword(e.target.value);
+  }
 
   return (
     <div>
@@ -31,13 +53,13 @@ const Home = (props) => {
 
               <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                  <Modal.Title>Modal heading</Modal.Title>
+                  <Modal.Title>Sign Up</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                   <Form>
                     <Form.Group controlId="formBasicEmail">
                       <Form.Label>Email address</Form.Label>
-                      <Form.Control type="email" placeholder="Enter email" />
+                      <Form.Control type="email" placeholder="Enter email" onChange={onEmailChange} />
                       <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                       </Form.Text>
@@ -45,7 +67,7 @@ const Home = (props) => {
 
                     <Form.Group controlId="formBasicPassword">
                       <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" />
+                      <Form.Control type="password" placeholder="Password" onChange={onPasswordChange} />
                     </Form.Group>
                     <Form.Group controlId="formBasicCheckbox">
                       <Form.Check type="checkbox" label="Check me out" />
